b57ba6b3415ea62c98146cf63c705dac010d1410
mriant <mriant@student.42.fr>
2023-06-26 19:42:51 +0200

    Starts Mariadb config
     Modifications qui seront validées :
    	modifié :         srcs/requirements/mariadb/Dockerfile
    	nouveau fichier : srcs/requirements/mariadb/conf/50-server.cnf
    	nouveau fichier : srcs/requirements/mariadb/tools/start_mariadb.sh
    	modifié :         srcs/requirements/nginx/Dockerfile

---
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -x
+
+service mariadb start
+# service start mariadb
+sleep 30
+
+mysql -e "CREATE USER IF NOT EXISTS \`${MYDB_USER}\`@'localhost' IDENTIFIED BY '${MYDB_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MY_DATABASE}\`.* TO \`${MYDB_USER}\`@'%' IDENTIFIED BY '${MYDB_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MY_ROOT_PASSWORD';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MY_DATABASE}\`;"
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
+
+# set +x
