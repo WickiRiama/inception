67cb6887502912115102b61c1fad8754bdb229a6
mriant <mriant@student.42.fr>
2023-08-08 17:28:10 +0200

    Still not working, still don't know why
     Modifications qui seront validées :
    	modifié :         srcs/docker-compose.yml
    	modifié :         srcs/requirements/mariadb/tools/start_mariadb.sh
    	modifié :         srcs/requirements/wordpress/Dockerfile
    	modifié :         srcs/requirements/wordpress/conf/wp_autoconfig.sh

--- srcs/requirements/mariadb/tools/start_mariadb.sh
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -4,11 +4,11 @@
 
 service mariadb start
 # service start mariadb
-sleep 20
+sleep 10
 
 mysql -e "CREATE USER IF NOT EXISTS \`${MYDB_USER}\`@'%' IDENTIFIED BY '${MYDB_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MY_DATABASE}\`.* TO \`${MYDB_USER}\`@'%' IDENTIFIED BY '${MYDB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MY_ROOT_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'wordpress' IDENTIFIED BY '$MY_ROOT_PASSWORD';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MY_DATABASE}\`;"
--- srcs/requirements/wordpress/conf/wp_autoconfig.sh
+++ srcs/requirements/wordpress/conf/wp_autoconfig.sh
@@ -2,14 +2,30 @@
 
 sleep 10 # Wait for MariaDB to start
 
-if [ ! -f wp-config.php ]
+if [ ! -f /var/www/html/wordpress/wp-config.php ]
 then
     wp config create --allow-root \
                      --dbname=$MY_DATABASE \
                      --dbuser=$MYDB_USER \
                      --dbpass=$MYDB_PASSWORD \
                      --dbhost=mariadb:3306 \
-                     --path='/var/www/wordpress'
+                     --path='/var/www/html/wordpress'
+    sleep 5
+    wp core install --url=mriant.42.fr \
+                    --title=INCEPTION \
+                    --admin_user=$WP_ADMIN_USER \
+                    --admin_password=$WP_ADMIN_PASSWORD \
+                    --admin_email=$WP_ADMIN_EMAIL \
+                    --allow-root \
+                    --path='/var/www/html/wordpress'
+
+
+    wp user create $WP_USER $WP_USER_EMAIL --allow-root \
+                   --user_pass=$WP_USER_PASSWORD \
+                   --role=$WP_USER_ROLE \
+                   --porcelain \
+                   --path='/var/www/html/wordpress'
+
 fi
 
-php-fpm7.4 -F
+exec php-fpm7.4 -F
