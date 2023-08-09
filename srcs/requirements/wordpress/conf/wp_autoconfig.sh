dc6ebc8f9019a43c995dcd8797dc26b540591fd4
mriant <mriant@student.42.fr>
2023-08-09 12:12:13 +0200

    Add env_file to wordpress container, correct other bugs
     Modifications qui seront validées :
    	modifié :         srcs/docker-compose.yml
    	modifié :         srcs/requirements/nginx/conf/nginx.conf
    	modifié :         srcs/requirements/wordpress/Dockerfile
    	modifié :         srcs/requirements/wordpress/conf/wp_autoconfig.sh

--- srcs/requirements/wordpress/conf/wp_autoconfig.sh
+++ srcs/requirements/wordpress/conf/wp_autoconfig.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-sleep 10 # Wait for MariaDB to start
+# sleep 10 # Wait for MariaDB to start
 
+echo coucou
 if [ ! -f /var/www/html/wordpress/wp-config.php ]
 then
+    cd /var/www/html/wordpress
+    echo a
     wp config create --allow-root \
                      --dbname=$MY_DATABASE \
                      --dbuser=$MYDB_USER \
                      --dbpass=$MYDB_PASSWORD \
-                     --dbhost=mariadb:3306 \
-                     --path='/var/www/html/wordpress'
-    sleep 5
+                     --dbhost=mariadb:3306
+    # sleep 5
+    echo b
     wp core install --url=mriant.42.fr \
                     --title=INCEPTION \
                     --admin_user=$WP_ADMIN_USER \
                     --admin_password=$WP_ADMIN_PASSWORD \
                     --admin_email=$WP_ADMIN_EMAIL \
-                    --allow-root \
-                    --path='/var/www/html/wordpress'
-
-
-    wp user create $WP_USER $WP_USER_EMAIL --allow-root \
+                    --skip-email \
+                    --allow-root
+    echo c
+    wp user create $WP_USER $WP_USER_EMAIL \
                    --user_pass=$WP_USER_PASSWORD \
                    --role=$WP_USER_ROLE \
                    --porcelain \
-                   --path='/var/www/html/wordpress'
-
+                   --allow-root
+    echo d
 fi
 
 exec php-fpm7.4 -F
